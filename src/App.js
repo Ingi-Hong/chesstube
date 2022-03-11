966dd7c1d128e872cf5bc3c019f4e0976fb5b8b8
Ingi-Hong <ingihong@bu.edu>
2022-03-10 19:15:57 -0500

    Deletions and making test json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import './videos.json';
 
 function App() {
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('videos.json')
+    .then((res) => res.json())
+    .then(
+      (result) => {
+        setIsLoaded(true);
+        setItems(result);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    );
+  }, []);
+
+  if (error){
+    return <>{error.message}</>;
+  } else if (!isLoaded) {
+    return <>loading...</>;
+  } else {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Hello</h1>
+
     </div>
   );
+}
 }
 
 export default App;
